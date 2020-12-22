c142ced5c14efd2917af477bef0d8c1755fdf22f
jinxul <ahmadgivekesh@gmail.com>
2020-12-22 21:03:15 +0330

    fix backButton bug in ThreadsFragment

--- app/src/main/java/com/givekesh/messages/ui/MainActivity.kt
+++ app/src/main/java/com/givekesh/messages/ui/MainActivity.kt
@@ -27,7 +27,12 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.PermissionsFragment,
+                R.id.ThreadsFragment
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
 
     }
